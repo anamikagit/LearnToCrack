6833ad7a1d1acdd0d9c4e9ba1148e842e6d49ee7
anamikagit <anamika.arya1990@gmail.com>
2017-02-18 07:10:03 -0800

    creating first appearance of app

--- app/src/main/java/com/example/aarya/learntocrack/model/Subject.java
+++ app/src/main/java/com/example/aarya/learntocrack/model/Subject.java
@@ -3,13 +3,23 @@ package com.example.aarya.learntocrack.model;
 public class Subject {
     private String name;
     private int numOfQuest;
+    private int thumbnail;
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
 
     public Subject() {
     }
 
-    public Subject(String name, int numOfQuest) {
+    public Subject(String name, int numOfQuest,int thumbnail) {
         this.name = name;
         this.numOfQuest = numOfQuest;
+        this.thumbnail = thumbnail;
     }
 
     public int getNumOfQuest() {
--- app/src/main/java/com/example/aarya/learntocrack/model/Topics.java
+++
@@ -1,4 +0,0 @@
-package com.example.aarya.learntocrack.model;
-
-public class Topics {
-}
